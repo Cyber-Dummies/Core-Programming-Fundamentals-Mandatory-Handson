8f20b3f18fcf45f717244ad56f693431515f5e3b
Kumar Ankit <98142050+Cyber-Dummies@users.noreply.github.com>
2022-01-21 17:29:46 +0530

    Create Library.java

---
+++ Book Manipulation/Library.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Library
+{
+    private ArrayList<Book> bookList = new ArrayList<Book>();
+    public void setBookList(ArrayList<Book> bookList){
+        this.bookList=bookList;
+    }
+    public ArrayList<Book> getBookList()
+    {
+        return bookList;
+    }
+    public void addBook(Book bobj)
+    {
+        bookList.add(bobj);
+    }
+    public boolean isEmpty()
+    {
+        return bookList.isEmpty();
+    }
+    public ArrayList<Book> viewAllBooks()
+    {
+        return bookList;
+    }
+    public ArrayList<Book> viewBooksByAuthor(String author)
+    {
+        ArrayList<Book> result=new ArrayList<Book>();
+        Iterator itr = bookList.iterator();
+        while(itr.hasNext())
+        {
+            Book b=(Book) itr.next();
+            if(b.getAuthor().equalsIgnoreCase(author))
+                result.add(b);
+        }
+        return result;
+    }
+    public int countnoofbook(String bname)
+    {
+        int count=0;
+        Iterator itr1=bookList.iterator();
+        while(itr1.hasNext())
+        {
+            Book b=(Book) itr1.next();
+            if(b.getBookName().equalsIgnoreCase(bname))
+                count++;
+        }
+        return count;
+    }
+}
